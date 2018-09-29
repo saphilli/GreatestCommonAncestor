b779e57d68191b14eb7f7b9a0b2b627b4e091c22
Sarah Phillips <Sarah@Louies-MacBook-Pro.local>
2018-09-29 13:21:58 +0100

    clean up testing code (can't unit test in playgrounds turns out)

--- LowestCommonAncester.playground/Contents.swift
+++ LowestCommonAncester.playground/Contents.swift
@@ -23,15 +23,26 @@ extension BinaryTree: CustomStringConvertible {
     }
 }
 
+func testTreeStructure() {
+    let node6 = BinaryTree.node(.empty,"6",.empty)
+    let node4 = BinaryTree.node(.empty, "4", .empty)
+    let node3 = BinaryTree.node(.empty, "3", node6)
+    let node7 = BinaryTree.node(.empty, "7", .empty)
+    let node2 = BinaryTree.node(node7, "2", node3)
+    let node10 = BinaryTree.node(node4, "10", .empty)
+    let root = BinaryTree.node(node2, "5", node10)
+    print(root) // 5 is root, 6 is lowest leaf node
+    if (root.count == 7) { //check number of nodes
+        print("Test Passed")
+        } else { print("Test Failed") }
+}
 
-let node6 = BinaryTree.node(.empty,"6",.empty)
-let node4 = BinaryTree.node(.empty, "4", .empty)
-let node3 = BinaryTree.node(.empty, "3", node6)
-let node7 = BinaryTree.node(.empty, "7", .empty)
-let node2 = BinaryTree.node(node7, "2", node3)
-let node10 = BinaryTree.node(node4, "10", .empty)
-let root = BinaryTree.node(node2, "5", node10)
-print(root) // 5 is root, 6 is lowest leaf node
-print(root.count) //should be 7
+testTreeStructure()
+
+
+
+
+
+
 
 
