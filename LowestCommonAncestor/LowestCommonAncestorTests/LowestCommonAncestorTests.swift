3e09235d99a7c7c2b14e6c68c3b4178ac6018320
Sarah Phillips <Sarah@Louies-MacBook-Pro.local>
2018-09-30 21:38:26 +0100

    more detailed tests, all successful so far

--- LowestCommonAncestor/LowestCommonAncestorTests/LowestCommonAncestorTests.swift
+++ LowestCommonAncestor/LowestCommonAncestorTests/LowestCommonAncestorTests.swift
@@ -12,8 +12,6 @@ import XCTest
 class LowestCommonAncestorTests: XCTestCase {
 
     func testTreeStructure() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
         let node6 = Node(left:nil,val: 6, right: nil)
         let node3 = Node(left:nil,val: 3, right: nil)
         let node9 = Node(left:nil,val:9,right:nil)
@@ -24,23 +22,30 @@ class LowestCommonAncestorTests: XCTestCase {
         let tree = BinaryTree (root: node5)
         XCTAssertEqual(tree.countNodes(n : tree.root),7)
         let tree1 = BinaryTree(root: nil)
-        XCTAssertEqual(tree.countNodes(n : tree1.root),0)
-        
-//        print(tree.toString(n: tree.root))
-//        print(tree.LCA(n1:9,n2:8)!.val)
+        XCTAssertEqual(tree.countNodes(n : tree1.root),0) 
     }
-
-    func testPerformanceExample() {
+    func testLCA() {
         let node6 = Node(left:nil,val: 6, right: nil)
         let node3 = Node(left:nil,val: 3, right: nil)
         let node9 = Node(left:nil,val:9,right:nil)
         let node4 = Node(left:nil,val: 4, right: node6)
-        let node8 = Node(left:node3,val: 8, right: node9)
-        let node2 = Node(left:node4,val: 2, right: nil)
+        let node8 = Node(left:node3,val: 8, right: nil)
+        let node2 = Node(left:node4,val: 2, right: node9)
         let node5 = Node (left: node2,val: 5,right: node8)
         let tree = BinaryTree (root: node5)
-        self.measure {
-            XCTAssertEqual(tree.countNodes(n : tree.root),7)
-        }
+        XCTAssertEqual(tree.LCA(n1:2,n2:3)?.val,5)
+        XCTAssertEqual(tree.LCA(n1:4,n2:9)?.val,2)
+        XCTAssertEqual(tree.LCA(n1:5,n2:5)?.val,5)
+        XCTAssertEqual(tree.LCA(n1:4,n2:6)?.val,4)
+        XCTAssertEqual(tree.LCA(n1:2,n2:8)?.val,5)
+        XCTAssertEqual(tree.LCA(n1:9,n2:6)?.val,2)
+        XCTAssertEqual(tree.LCA(n1:5,n2:5)?.val,5)
+        XCTAssertEqual(tree.LCA(n1:9,n2:3)?.val,5)
+        XCTAssertEqual(tree.LCA(n1:666,n2:420)?.val,nil)
+    }
+    func testLCANil() {
+        let tree = BinaryTree( root: nil)
+        XCTAssertEqual(tree.LCA(n1:2,n2:1)?.val,nil)
+        
     }
 }
