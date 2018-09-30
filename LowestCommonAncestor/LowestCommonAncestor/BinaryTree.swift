fa9cf2f038eb1e379ab1e08536f21397c16a70c6
Sarah Phillips <Sarah@Louies-MacBook-Pro.local>
2018-09-30 22:29:58 +0100

    All tests successful, code coverage report at 100%

--- LowestCommonAncestor/LowestCommonAncestor/BinaryTree.swift
+++ LowestCommonAncestor/LowestCommonAncestor/BinaryTree.swift
@@ -3,25 +3,28 @@
 //  LowestCommonAncestor
 //
 //  Created by Sarah Phillips on 29/09/2018.
+//  Student Number:16318649
 //  Copyright © 2018 Sarah Phillips. All rights reserved.
 //
-
+// Node class which the Binary Tree class will consist of
 class Node {
-    var val: Int
+    var val: Int //the data the node contains
     var left: Node?
-    var right: Node? //can be nodes or nil values
+    var right: Node? //can be a node or nil value
     init(left:Node?,val: Int,right:Node?) {
         self.val = val
         self.left = left
         self.right = right
     }
 }
+//Binary Tree class conisting of nodes
 class BinaryTree{
-    var root: Node?
+    var root: Node? //highest node of the tree
     init(root: Node?) {
         self.root = root
     }
-    
+    //function to count the number of nodes in the binary tree rooted at Node n
+    //takes in an Optional Node n and returns an Int
     func countNodes(n:Node?) -> Int {
         var count : Int
         if (n == nil) { return 0 }
@@ -29,20 +32,23 @@ class BinaryTree{
         return (count)
         
     }
+    //converts the binary tree rooted at Node N into a string to display contents of each node
+    //takes in an Optional Node n and returns a String
     func toString(n: Node?) -> String {
         var tree : String
         if (n == nil) { return "_" }
         tree = "value: \(n!.val), left = (\(toString(n: n!.left ))), right = (\(toString(n: n?.right)))"
         return tree
     }
+    //recursive function to find the lowest common ancestor of nodes n1 and n2 which are nodes of a binary tree
+    //takes in Nodes n1, n2 and returns an Optional Node
     func LCA(n1: Int,n2:Int) -> Node? {
         return findLCA(n:root,n1:n1,n2:n2)
     }
+    
     func findLCA(n:Node?, n1:Int,n2:Int) -> Node? {
-        if n ==  nil {
-            return nil
-        }
-        if(n!.val == n1 || n!.val == n2) { return n }
+        if n ==  nil { return nil }
+        if(n!.val == n1 || n!.val == n2) { return n } 
         let left_lca = findLCA(n:n!.left,n1:n1,n2:n2)
         let right_lca = findLCA(n:n!.right,n1:n1,n2:n2)
         if (left_lca != nil && right_lca != nil) { return n }
--- LowestCommonAncestor/LowestCommonAncestorTests/LowestCommonAncestorTests.swift
+++ LowestCommonAncestor/LowestCommonAncestorTests/LowestCommonAncestorTests.swift
@@ -3,6 +3,7 @@
 //  LowestCommonAncestorTests
 //
 //  Created by Sarah Phillips on 29/09/2018.
+//  Student Number:16318649
 //  Copyright © 2018 Sarah Phillips. All rights reserved.
 //
 
@@ -11,11 +12,13 @@ import XCTest
 
 class LowestCommonAncestorTests: XCTestCase {
 
+    //Test the general structure of a binary tree and whether count is correct
+    //also tests for nill and negative Int inputs
     func testTreeStructure() {
         let node6 = Node(left:nil,val: 6, right: nil)
-        let node3 = Node(left:nil,val: 3, right: nil)
+        let node3 = Node(left:nil,val: -3, right: nil)
         let node9 = Node(left:nil,val:9,right:nil)
-        let node4 = Node(left:nil,val: 4, right: node6)
+        let node4 = Node(left:nil,val: -4, right: node6)
         let node8 = Node(left:node3,val: 8, right: nil)
         let node2 = Node(left:node4,val: 2, right: node9)
         let node5 = Node (left: node2,val: 5,right: node8)
@@ -24,7 +27,9 @@ class LowestCommonAncestorTests: XCTestCase {
         let tree1 = BinaryTree(root: nil)
         XCTAssertEqual(tree.countNodes(n : tree1.root),0) 
     }
-    func testPrint() {
+    //test to see whether the structure of the binary tree is correctly represented by toString() method
+    //tests for nil tree
+    func testToString() {
         let node6 = Node(left:nil,val: 6, right: nil)
         let node3 = Node(left:nil,val: 3, right: nil)
         let node9 = Node(left:nil,val:9,right:nil)
@@ -33,8 +38,12 @@ class LowestCommonAncestorTests: XCTestCase {
         let node2 = Node(left:node4,val: 2, right: node9)
         let node5 = Node (left: node2,val: 5,right: node8)
         let tree = BinaryTree (root: node5)
-        print(tree.toString(n: tree.root))
+        XCTAssertEqual(tree.toString(n: tree.root),"value: 5, left = (value: 2, left = (value: 4, left = (_), right = (value: 6, left = (_), right = (_))), right = (value: 9, left = (_), right = (_))), right = (value: 8, left = (value: 3, left = (_), right = (_)), right = (_))")
+        XCTAssertEqual(tree.toString(n:node2),"value: 2, left = (value: 4, left = (_), right = (value: 6, left = (_), right = (_))), right = (value: 9, left = (_), right = (_))")
+        let tree2 = BinaryTree(root: nil)
+        XCTAssertEqual(tree2.toString(n:tree2.root),"_")
     }
+    //test for LCA function
     func testLCA() {
         let node6 = Node(left:nil,val: 6, right: nil)
         let node3 = Node(left:nil,val: 3, right: nil)
@@ -52,11 +61,11 @@ class LowestCommonAncestorTests: XCTestCase {
         XCTAssertEqual(tree.LCA(n1:9,n2:6)?.val,2)
         XCTAssertEqual(tree.LCA(n1:5,n2:5)?.val,5)
         XCTAssertEqual(tree.LCA(n1:9,n2:3)?.val,5)
-        XCTAssertEqual(tree.LCA(n1:666,n2:420)?.val,nil)
+        XCTAssertEqual(tree.LCA(n1:667696,n2:4247860)?.val,nil)
     }
-    func testLCANil() {
+    //tests the nil case of the LCA function
+    func testNullCaseLCA() {
         let tree = BinaryTree( root: nil)
         XCTAssertEqual(tree.LCA(n1:2,n2:1)?.val,nil)
-        
     }
 }
