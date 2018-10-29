eb72b52f155976b5221aa4d9c8bdbbae53443ed1
Sarah Phillips <Sarah@Louies-MacBook-Pro.local>
2018-10-29 17:49:20 +0000

    Not enough time to figure out structs and protcols, changed to classes again

--- LowestCommonAncestor/LowestCommonAncestor/Edge.swift
+++ LowestCommonAncestor/LowestCommonAncestor/Edge.swift
@@ -2,36 +2,20 @@
 //  Edge.swift
 //  LowestCommonAncestor
 //
-//  Created by Sarah Phillips on 24/10/2018.
+//  Created by Sarah Phillips 
 //  Copyright © 2018 Sarah Phillips. All rights reserved.
 //
 
 import Foundation
-//edge conforms to Hashable protocol which conforms to Equatable protocol
-public struct Edge<T:Hashable> {
-    public let from: Vertex<T> //vertex the edge starts from
-    public let to: Vertex<T> //vertex the edge travels to
-}
-//make edge type printable
-extension Edge: CustomStringConvertible {
-    public var description: String {
-        return "\(from.description) -> \(to.description)" //if w is nil
+
+public class Edge<T>: CustomStringConvertible {
+    var from: Vertex<T>
+    var to: Vertex<T>
+    init(_ src:Vertex<T>,_ dst:Vertex<T>) {
+        from = src
+        to = dst
     }
-}
-//conform to hashable protocol by providing hashValue
-extension Edge: Hashable {
-    public var hashValue: Int {
-        let string = "\(from.description)\(to.description)"
-        return string.hashValue
+    public var description: String{
+        return "(\(from):\(to))"
     }
-}
-//provide equatable function to conform to equatable protocol
-public func == <T>(lhs: Edge<T>, rhs: Edge<T>) -> Bool {
-    if lhs.from != rhs.from {
-        return false
-    }
-    if lhs.to != rhs.to {
-        return false
-    }
-    return true
 }
