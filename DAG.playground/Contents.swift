41d79d8920edb20e12db49a0b250138ccac65edb
Sarah Phillips <Sarah@Louies-MacBook-Pro.local>
2018-10-29 18:21:41 +0000

    Forgot to push playground file

--- DAG.playground/Contents.swift
+++ DAG.playground/Contents.swift
@@ -1,66 +1,66 @@
-import Foundation
 
-public struct Vertex<T: Hashable> {
-    public let index: Int
-    public var data: T
-   
-}
 
-extension Vertex: CustomStringConvertible { //allow to be printed as a string
-    public var description: String {
-        return "\(index): \(data)"
+public class Vertex<T> : CustomStringConvertible{
+    var key: Int
+    var data: T
+    var neighbours: Array<Edge<T>>
+    init(_ key:Int,_ data:T) {
+        self.key = key
+        self.data = data
+        self.neighbours = Array<Edge<T>>()
     }
-}
-
-extension Vertex: Hashable { //conform to hashable protocol
-    public var hashValue: Int {
-        return "\(data)\(index)".hashValue
+    public var description: String{
+        return "(\(key):\(data))"
     }
 }
 
-public func ==<T>(lhs: Vertex<T>, rhs: Vertex<T>) -> Bool { // conform to equatable protocol
-    if lhs.index != rhs.index
-    {
-        return false
+public class Edge<T>: CustomStringConvertible {
+    var from: Vertex<T>
+    var to: Vertex<T>
+    init(_ src:Vertex<T>,_ dst:Vertex<T>) {
+        from = src
+        to = dst
     }
-    if lhs.data == rhs.data
-    {
-        return false;
+    public var description: String{
+        return "(\(from):\(to))"
     }
-    return true;
+}
+//printable queue class
+public class Queue<T> : CustomStringConvertible{
+    var array = [T]()
+    public var description: String{
+        var str = ""
+        for i in array {
+            str+="\(i) "
+        }
+        return "\(str)"
+    }
+    public var count: Int {
+        return array.count
+    }
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    public func enqueue(_ element: T) {
+        array.append(element)
+    }
+    public func dequeue() -> T? {
+        if isEmpty {
+            return nil
+        } else {
+            return array.removeFirst()
+        }
+    }
 }
 
-import Foundation
-//edge conforms to Hashable protocol which conforms to Equatable protocol
-public struct Edge<T:Hashable> {
-    public let from: Vertex<T> //vertex the edge starts from
-    public let to: Vertex<T> //vertex the edge travels to
-}
-//make edge type printable
-extension Edge: CustomStringConvertible {
-    public var description: String {
-        return "\(from.description) -> \(to.description)" //if w is nil
-    }
-}
-//conform to hashable protocol by providing hashValue
-extension Edge: Hashable {
-    public var hashValue: Int {
-        let string = "\(from.description)\(to.description)"
-        return string.hashValue
-    }
-}
-//provide equatable function to conform to equatable protocol
-public func == <T>(lhs: Edge<T>, rhs: Edge<T>) -> Bool {
-    if lhs.from != rhs.from {
-        return false
-    }
-    if lhs.to != rhs.to {
-        return false
-    }
-    return true
-}
-
-var v1 = Vertex(index: 0, data: 2.0)
-var v2 = Vertex(index: 1, data: 5.6)
-var edge = Edge(from: v1, to: v2)
-print(edge)
+var queue = Queue<Int>()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+print(queue)
+queue.dequeue()
+print(queue)
+queue.dequeue()
+print(queue)
+print(queue.count)
